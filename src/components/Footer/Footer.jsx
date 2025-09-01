f22fcd7506704256e6172a9e6d56e005b9244173
Craig Sampson <craigsjr15@gmail.com>
2025-08-31 20:21:09 -0400

    footer size update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-white text-gray-800 py-4">
+    <footer className="bg-white text-gray-800 py-4 border-t">
       <div className="container mx-auto text-center">
-        <p className="text-sm">
+        <p className="text-sm md:text-lg lg:text-xl">
           &copy; {new Date().getFullYear()} Craig Sampson. All rights reserved.
         </p>
       </div>
